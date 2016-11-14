9d3c472e942734152515a4b0430d181b2bbc6a04
ALEXANDRE <a.martin@ludus-academie.com>
2016-11-14 17:36:13 +0100

    tp croix

--- TP_CROIX.pas
+++ TP_CROIX.pas
@@ -1,9 +1,9 @@
 program TP_CROIX;
-
+//But : afficher une croix
+//Entree : taille de la croix et le caractere utiliser
+//Sortie : la croix
 uses crt;
 
-CONST
-	ESPACE= ' '; //constante pour ajouter des espaces
 VAR
 	nombre,i,j: integer;
 	signe :char;
@@ -18,11 +18,10 @@ BEGIN
 			begin
 				for j:=1 to nombre do
 				begin
-					IF (i=j) OR (i+j-1 = nombre) 
-					THEN
+					IF (i=j) OR (i+j-1 = nombre) THEN
 					write(signe)
-					ELSE
-					write(ESPACE)
+				ELSE
+					write(' ')
 				END;
 				writeln;
 			END;
@@ -30,3 +29,30 @@ readln;
 
 END.
 
+
+(*
+//But : afficher une croix
+//Entree : taille de la croix et le caractere utiliser
+//Sortie : la croix
+
+VAR
+	nombre,i,j: ENTIER;
+	signe :CARACTERE;
+
+DEBUT	
+	ECRIRE "enter le signe"
+	LIRE signe
+	ECRIRE "entre la taille de la croix"
+	LIRE nombre
+	POUR i<-1 A nombre FAIRE
+	DEBUT
+		POUR j<-1 A nombre FAIRE
+		DEBUT
+			SI (i=j) OU (i+j-1 = nombre) ALORS
+			ECRIRE(signe)
+		SINON
+			ECRIRE(' ')
+		FIN SI;
+	FIN SI;
+FIN
+*)
